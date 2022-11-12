8c4b86eb89cc43979f68eec59111f092778c940b
Robby Lewis <Robbysim37@gmail.com>
2022-11-11 22:21:55 -0500

    Added functionality to character component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useState,useEffect} from 'react';
+import CharacterBackground from './components/CharacterBackground';
+import Character from './components/Character';
+
+const SWAPIPeopleurl = "https://swapi.dev/api/people/"
+
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -6,11 +13,25 @@ const App = () => {
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  // sync up with, if any.  
+  const [SWAPIPeople, setSWAPIPeople] = useState()
+
+  useEffect(() =>{
+    axios.get(SWAPIPeopleurl).then((res) =>{
+      console.log(res.data)
+      setSWAPIPeople(res.data)
+    })},[])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {SWAPIPeople && <CharacterBackground>
+      {SWAPIPeople.map((el,i) => {
+        console.log(el.name)
+        return(
+          <Character arrCount={SWAPIPeople.length} id={i} key={i} characterObj={el}/>
+        )
+      })}</CharacterBackground>}
     </div>
   );
 }
